2a4885d881ba372e28b88e28bc46d64bc8f00b9d
gonyango <grantononyango@gmail.com>
2024-08-26 21:55:07 +0300

    feat: isaac newton

---
+++ physics.js
@@ -0,0 +1,11 @@
+function getAcceleration(obj1){
+    if (obj1.F != undefined && obj1.m != undefined){
+        return obj1.F / obj1.m
+    } else if (obj1.Δv != undefined && obj1.Δt != undefined){
+        return obj1.Δv / obj1.Δt
+    } else if (obj1.d != undefined && obj1.t != undefined){
+        return (2 * obj1.d) / (obj1.t * obj1.t)
+    } else {
+        return "impossible"
+    }
+}
