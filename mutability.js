b9ac0b9bdad1d1c9ef460dbd3d6bff90ba73a969
gonyango <grantononyango@gmail.com>
2024-08-26 12:49:18 +0300

    feat: mutability of classes js

--- mutability.js
+++ mutability.js
@@ -1,9 +1,15 @@
 const person = {
     name: 'Rick',
-    age: 78,
-    country: 'FR',
-  }
+    age: 77,
+    country: 'US',
+}
 
 const clone1 = Object.assign({},person)
 const clone2 =  Object.assign({},person)
+person.age = 78
+person.country = 'FR'
 const samePerson = Object.assign({},person)
+
+console.log(clone1)
+console.log(clone2)
+console.log(samePerson)
