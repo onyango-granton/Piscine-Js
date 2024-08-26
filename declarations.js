7b95555079c72589c5c811b28f1ac7e73bdc7c21
gonyango <grantononyango@gmail.com>
2024-08-26 11:39:19 +0300

    feat: declarations js

--- declarations.js
+++ declarations.js
@@ -1,4 +1,4 @@
-const escapeStr = "`, \, /,\"'"
+const escapeStr = "`, \\ , /,\"'"
 const arr = [4, "2"]
 const obj = {str:"hamza", num:28, bool: true, undef: undefined}
 const nested = {arr:[4,undefined,"2"],obj:{str:"yusuf",num:45,bool:true}}
