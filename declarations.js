4ee73ccc301817d8ed4227d24b883fa81732f47f
gonyango <grantononyango@gmail.com>
2024-08-26 12:00:32 +0300

    feat: declarations js file

--- declarations.js
+++ declarations.js
@@ -1,7 +1,6 @@
 const escapeStr = "` , \\ , /,\"'"
 const arr = [4, "2"]
 const obj = {str:"hamza", num:28, bool: true, undef: undefined}
-delete obj.undef
 const nested = {arr:[4,undefined,"2"],obj}
 Object.freeze(arr)
 Object.freeze(nested)
