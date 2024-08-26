78a471b6193d1ab2384241554b126c4355d03aa2
gonyango <grantononyango@gmail.com>
2024-08-26 18:29:38 +0300

    arrays as objects

--- is.js
+++ is.js
@@ -1,4 +1,4 @@
-is.obj = (value) => !(is.arr(value))
+is.obj = (value) => !(is.arr(value)) && typeof(value) === 'object'
 is.num = (value) => typeof(value) === 'number' 
 is.nan = (value) => Number.isNaN(value) 
 is.str = (value) => typeof(value) === 'string'
