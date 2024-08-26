ff7e40236aa722b74f8849561bc678ea2069f9a2
gonyango <grantononyango@gmail.com>
2024-08-26 21:57:24 +0300

    is defined func

--- physics.js
+++ physics.js
@@ -1,6 +1,6 @@
 function getAcceleration(obj1){
-    if (obj1.F != undefined && obj1.m != undefined){
-        return obj1.F / obj1.m
+    if (obj1.f != undefined && obj1.m != undefined){
+        return obj1.f / obj1.m
     } else if (obj1.Δv != undefined && obj1.Δt != undefined){
         return obj1.Δv / obj1.Δt
     } else if (obj1.d != undefined && obj1.t != undefined){
