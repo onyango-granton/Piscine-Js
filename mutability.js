bbc758d398cffadf97b4213e5721246470d86cea
gonyango <grantononyango@gmail.com>
2024-08-26 13:08:39 +0300

    person has already been declared

--- mutability.js
+++ mutability.js
@@ -1,12 +1,5 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-}
-
 const clone1 = {...person}
 const clone2 =  {...person}
-const samePerson = {person}
-
+const samePerson = person
 person.age = 78
 person.country = 'FR'
