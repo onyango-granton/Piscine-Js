24085e773bb54ac1fbc3947201b1725b8f518484
gonyango <grantononyango@gmail.com>
2024-08-26 13:14:21 +0300

    feat: functions in js

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(num1){
+    return num1 + 1
+}
+
+function less(num1){
+    return num1 - 1
+}
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+function sub(num1, num2){
+    return num1 - num2
+}
