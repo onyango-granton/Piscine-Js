6d6e742f8c527f86f5a23c2c49ba274e9e2e1f94
gonyango <grantononyango@gmail.com>
2024-08-26 16:34:26 +0300

    is was redeclared

--- is.js
+++ is.js
@@ -1,13 +1,12 @@
-const is = {
-    num : (value) => typeof(value) === 'number' ? true : false,
-    nan : (value) => typeof(value) === 'Nan' ? true : false,
-    str : (value) => typeof(value) === 'string' ? true :false,
-    bool : (value) => typeof(value) === 'boolean' ? true : false,
-    undef: (value) => typeof(value) === 'undefined' ? true : false,
-    def : (value) => typeof(value) === 'defined' ? true : false,
-    arr : (value) => typeof(value) === 'array' ? true: false,
-    obj : (value) => typeof(value) === 'object' ? true : false,
-    fun : (value) => typeof(value) === 'function' ? true : false,
-    truthy : (value) => typeof(value) === 'truthy' ? true : false,
-    falsy : (value) => typeof(value) === "falsy" ? true : false,
-}
+is.num = (value) => typeof(value) === 'number' ? true : false
+is.nan = (value) => typeof(value) === 'Nan' ? true : false
+is.str = (value) => typeof(value) === 'string' ? true :false
+is.bool = (value) => typeof(value) === 'boolean' ? true : false
+is.undef= (value) => typeof(value) === 'undefined' ? true : false
+is.def = (value) => typeof(value) === 'defined' ? true : false
+is.arr = (value) => typeof(value) === 'array' ? true: false
+is.obj = (value) => typeof(value) === 'object' ? true : false
+is.fun = (value) => typeof(value) === 'function' ? true : false
+is.truthy = (value) => typeof(value) === 'truthy' ? true : false
+is.falsy = (value) => typeof(value) === "falsy" ? true : false
+
