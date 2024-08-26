4da08b9aa4e436392085015532ecc490de98d29d
gonyango <grantononyango@gmail.com>
2024-08-26 12:55:25 +0300

    person === sameperson

--- mutability.js
+++ mutability.js
@@ -6,10 +6,5 @@
 
 const clone1 = Object.assign({},person)
 const clone2 =  Object.assign({},person)
-person.age = 78
-person.country = 'FR'
-const samePerson = Object.assign({},person)
 
-console.log(clone1)
-console.log(clone2)
-console.log(samePerson)
+const samePerson = Object.assign({},person)
