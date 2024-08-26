96c5489213e04140ebfa63878ac07f0294b4030c
gonyango <grantononyango@gmail.com>
2024-08-26 13:42:26 +0300

    typeof function and deeply equal

--- concat-str.js
+++ concat-str.js
@@ -1,3 +1,9 @@
 function concatStr(arg1, arg2){
+    if (typeof(arg1) === 'number'){
+        arg1 = arg1.toString()
+    }
+    if (typeof(arg2) === 'number'){
+        arg2 = arg2.toString()
+    }
     return arg1 + arg2
 }
