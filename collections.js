ee2886ad65f628547df400811c522d5db9b31372
gonyango <grantononyango@gmail.com>
2024-08-27 08:54:24 +0300

    super type of function

--- collections.js
+++ collections.js
@@ -42,3 +42,62 @@ function objToMap(obj) {
 function arrToObj(arr) {
     return Object.assign({},arr)
 }
+
+// const is = {}
+
+// is.obj = (value) => !(is.arr(value)) && typeof(value) === 'object' && is.truthy(value)
+// is.num = (value) => typeof(value) === 'number' 
+// is.nan = (value) => Number.isNaN(value) 
+// is.str = (value) => typeof(value) === 'string'
+// is.bool = (value) => typeof(value) === 'boolean' 
+// is.undef= (value) => typeof(value) === 'undefined' 
+// is.def = (value) => !is.undef(value)
+// is.arr = (value) => Array.isArray(value)
+// is.fun = (value) => typeof(value) === 'function' 
+// is.falsy = (value) => {
+//     return value === false || value === 0 || value === -0 ||
+//            value === 0n || value === '' || value === null || 
+//            value === undefined || Number.isNaN(value);
+// }
+// is.truthy = (value) => !is.falsy(value)
+
+function superTypeOf(arg){
+    if (typeof(arg) === 'object' && arg.size !== undefined){
+        if (arg instanceof Map){
+            return 'Map'
+        } else if (arg instanceof Set){
+            return 'Set'
+        }     
+    }else if (typeof(arg) === 'object' ){
+        return 'Object'
+    } else if (typeof(arg) === 'string'){
+        return 'String'
+    } else if (typeof(arg) === 'number'){
+        return 'Number'
+    } else if (Number.isNaN(arg)){
+        return 'Number'
+    } else if (Array.isArray(arg)){
+        return 'Array'
+    } else if (arg === null) {
+        return 'null'
+    } else if (typeof(arg) === 'undefined'){
+        return 'undefined'
+    } else if (typeof(arg) === 'function'){
+        return 'Function'
+    }
+}
+
+// let map2 = new Map()
+
+// let obj = new Object()
+
+// let set2 = new Set([1,4,5,1,2,4])
+
+// set2.add
+
+// // Object.keys
+
+// console.log(superTypeOf(map2))
+// console.log(superTypeOf(obj))
+// console.log(superTypeOf(set2))
+// // console.log(set2.add())
