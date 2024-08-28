29f62211a75b3c702068d38e88c29526cb383f30
gonyango <grantononyango@gmail.com>
2024-08-28 16:02:58 +0300

    multiply function

--- elementary.js
+++ elementary.js
@@ -2,12 +2,21 @@ const multiply =  function(a,b){
     if (a == 0 || b == 0){
         return 0
     }
+    let sign = 1
+    if (a < 0){
+        a = Math.abs(a)
+        sign *= -1
+    }
+    if (b < 0){
+        b = Math.abs(b)
+        sign *= -1
+    }
     let res = a
     for (let i = 1; i < b; i ++){
         res += a
     }
 
-    return res
+    return sign * res
 
 }
 
