5e4933793c8f4549a5ce67b0e3bf51f5e4cce67e
gonyango <grantononyango@gmail.com>
2024-08-28 16:42:02 +0300

    working with arrays

---
+++ index-of.js
@@ -0,0 +1,31 @@
+const indexOf = function(array,value,index) {
+    if (index == null){
+        index = 0
+    }
+    for (let i = index; i < array.length;i++){
+        if (array[i] == value){
+            return i
+        }
+    }
+    return -1
+}
+
+
+const lastIndexOf = function(array,value,index){
+    let lastIndex
+    if (index == null){
+        index = 0
+    }
+    for (let i = index; i < array.length;i++){
+        if (array[i] == value){
+            lastIndex = i
+        }
+    }
+    if (lastIndex !== undefined){
+        return lastIndex
+    } else {
+        return -1
+    }
+}
+
+const includes = (array,value,index) => indexOf(array,value,index) == -1 ? false : true
