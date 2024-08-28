7396acce52b028cede9d7ed9a705396b01173dfb
gonyango <grantononyango@gmail.com>
2024-08-28 16:19:03 +0300

    modulo and div function

--- elementary.js
+++ elementary.js
@@ -2,22 +2,24 @@ const multiply =  function(a,b){
     if (a == 0 || b == 0){
         return 0
     }
-    let sign = 1
+    let sign = false
     if (a < 0){
         a = Math.abs(a)
-        sign *= -1
+        sign = !sign
     }
     if (b < 0){
         b = Math.abs(b)
-        sign *= -1
+        sign = !sign
     }
     let res = a
     for (let i = 1; i < b; i ++){
         res += a
     }
-
-    return sign * res
-
+    if (sign) {
+        return -res
+    } else {
+        return res
+    }
 }
 
 const divide = function(a,b){
@@ -30,12 +32,25 @@ const divide = function(a,b){
     if (b == 0){
         return Infinity
     }
+    let sign = false
+    if (a < 0){
+        a = Math.abs(a)
+        sign = !sign
+    }
+    if (b < 0){
+        b = Math.abs(b)
+        sign = !sign
+    }
     var times = 0
     while (a >= b) {
         a = a - b
         times++
     }
-    return times
+    if (sign) {
+        return -times
+    } else {
+        return times
+    }
 
 }
 
@@ -49,8 +64,20 @@ const modulo = function(a,b){
     if (b == 0){
         return NaN
     }
+    if (a < 0 && b < 0){
+        return -0
+    }
+    if (a < 0){
+        return -0
+    }
+    if (b < 0){
+        return 0
+    }
+
     while (a >= b) {
         a = a - b
     }
     return a
 }
+
+
