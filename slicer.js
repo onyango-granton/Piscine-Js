f9c69372a5c7130e70253bc5b4e674151afbea2e
gonyango <grantononyango@gmail.com>
2024-08-29 15:15:28 +0300

    slicer function js

--- slicer.js
+++ slicer.js
@@ -1,20 +1,11 @@
 const slice = function (string, start, end){
-    let res
-    if (typeof(string) === 'String'){
-        res = ""
-    } else {
-        res = new Array
-    }
+    let res = ""
     if (end == null && start != null){
         if (start < 0){
             start = string.length + start
         }
         for (let i = start; i< string.length; i++){
-            if (typeof(string) === 'String'){
-                res += string[i]
-            } else {
-                res.push(string[i])
-            }
+            res += string[i]
         }
         return res
     } else if (start != null && end != null){
@@ -22,11 +13,7 @@ const slice = function (string, start, end){
             end = string.length + end
         }
         for (let i = start; i< string.length; i++){
-            if (typeof(string) === 'String'){
-                res += string[i]
-            } else {
-                res.push(string[i])
-            }
+            res += string[i]
         }
         return res
     } else if (start == null && end == null) {
