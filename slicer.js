010f8071082a0bb1b5a7f9b5b979c2c4bd585dc8
gonyango <grantononyango@gmail.com>
2024-08-29 15:10:43 +0300

    slicer function js

---
+++ slicer.js
@@ -0,0 +1,24 @@
+const slice = function (string, start, end){
+    let res = new Array
+    if (end == null && start != null){
+        if (start < 0){
+            start = string.length + start
+        }
+        for (let i = start; i< string.length; i++){
+            res.push(string[i])
+        }
+        return res
+    } else if (start != null && end != null){
+        if (end < 0){
+            end = string.length + end
+        }
+        for (let i = start; i < end; i++){
+            res.push(string[i])
+        }
+        return res
+    } else if (start == null && end == null) {
+        return string
+    }
+    
+}
+
