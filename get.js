13301aec89f0f916273fd341be9a15545f30794d
gonyango <grantononyango@gmail.com>
2024-08-30 18:20:08 +0300

    nested object

--- get.js
+++ get.js
@@ -3,8 +3,13 @@ const get = function(src,path){
     let values = path.split('.')
     for (let i = 0; i < values.length; i++){
         res = src[values[i]]
+        if (res === undefined){
+            return undefined
+        }
         src = res
         
     }
     return res
 }
+
+console.log(get({ nested: { key: 'value' } }, 'nx.nx'))
