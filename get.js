50aa8aaa41e19112c23ec0c50c6749d0d8b2cd17
gonyango <grantononyango@gmail.com>
2024-08-30 18:14:20 +0300

    nested object

--- get.js
+++ get.js
@@ -1,6 +1,6 @@
 const get = function(src,path){
-    var res = ""
-    values = path.split('.')
+    let res = ""
+    let values = path.split('.')
     for (let i = 0; i < values.length; i++){
         res = src[values[i]]
         src = res
