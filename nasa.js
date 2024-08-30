a729622c4855f8b87a9ab5e1466cc47bfe0c6530
gonyango <grantononyango@gmail.com>
2024-08-30 16:23:20 +0300

    divisibility by 3,5

---
+++ nasa.js
@@ -0,0 +1,22 @@
+const nasa = function(num){
+    let res = ""
+    for (let i = 1; i <= num; i++){
+        if (i % 15 == 0){
+            res += " NASA"
+            continue
+        } else if (i % 5 == 0){
+            res += " SA"
+        } else if (i % 3 == 0){
+            res += " NA"
+        } else {
+            if (i == 1){
+                res += i.toString()
+            } else {
+                res += " "+i.toString()
+            }
+            
+        }
+        
+    }
+    console.log(res)
+}
