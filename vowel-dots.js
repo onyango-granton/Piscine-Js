10f93ad5a7103b0119a1c6839dbb58cee87fbdbc
gonyango <grantononyango@gmail.com>
2024-08-31 12:04:45 +0300

    rename variables

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,5 +1,5 @@
 const vowels = /[aeiou]/g;
 const vowelDots = function (string) {
-  string = string.replace(charset, (match) => match + ".");
+  string = string.replace(vowels, (match) => match + ".");
   return string;
 };
