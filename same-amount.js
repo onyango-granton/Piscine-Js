355c6e7cd78ee2304d5d98f7f0bd337c54e2fa62
gonyango <grantononyango@gmail.com>
2024-08-31 12:29:12 +0300

    varnaming

--- same-amount.js
+++ same-amount.js
@@ -1,9 +1,9 @@
 const sameAmount = function (str, reg1, reg2) {
-    match1 = str.match(reg1)
+    let match1 = str.match(reg1)
     if (match1 == null) {
         match1 = []
     }
-    match2 = str.match(reg2)
+    let match2 = str.match(reg2)
     if (match2 == null) {
         match2 = []
     }
