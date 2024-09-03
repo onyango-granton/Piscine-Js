ff06b0e4203cdbb3663ac5bb7adfaf33838b1524
gonyango <grantononyango@gmail.com>
2024-09-03 08:52:56 +0300

    time travel

--- dr-strange.js
+++ dr-strange.js
@@ -30,22 +30,22 @@ const addWeek = function (arg) {
 const timeTravel = function ({ date, hour, minute, second }) {
   let newDate = new Date()
   newDate.setTime(date.getTime());
-  newDate.setHours(hour - 2)
+  newDate.setHours(hour)
   newDate.setMinutes(minute)
   newDate.setSeconds(second)
-  newDate.setU
+  newDate.toLocaleString("en-GB", { timeZone: "Europe/London" });
   return newDate;
 };
 
-console.log(addWeek(new Date("0001-01-02")));
-console.log(addWeek(new Date("0001-01-08")));
+// console.log(addWeek(new Date("0001-01-02")));
+// console.log(addWeek(new Date("0001-01-08")));
 
 
-console.log(
-  timeTravel({
-    date: new Date("2020-05-29 23:25:22"),
-    hour: 21,
-    minute: 22,
-    second: 22,
-  }).toString()
-);
+// console.log(
+//   timeTravel({
+//     date: new Date("2020-05-29 23:25:22"),
+//     hour: 21,
+//     minute: 22,
+//     second: 22,
+//   }).toString()
+// );
