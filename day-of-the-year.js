06f2bc1b1788eeae0bc42281c9c706a4d4298d34
gonyango <grantononyango@gmail.com>
2024-09-04 08:40:49 +0300

    days since day one

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,1 +1,13 @@
-const dayOfTheYear = (date) => date.getDate() === 1 && date.getMonth() === 0 ? true : false
+const dayOfTheYear = function (date) {
+    let year = date.getFullYear()
+    let firstDate = new Date(`01-01-${year}`)
+    let timeElapse = date.getTime() - firstDate.getTime()
+
+    let daysElapsed = Math.round(timeElapse / (24 * 60 * 60 * 1000));
+
+    if (date.getDate() == 1 && date.getMonth() == 0) {
+        return 1
+    } else {
+        return daysElapsed + 1
+    }
+} 
