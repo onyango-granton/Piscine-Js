f2ac5252dbc5755be0d658893cc4c16fb91f0089
gonyango <grantononyango@gmail.com>
2024-09-04 08:11:09 +0300

    counting leap years

---
+++ count-leap-years.js
@@ -0,0 +1,19 @@
+const countLeapYears = function (date) {
+    const year = date.getFullYear();
+    
+    let countLeapYear = 0
+
+    for (let i = 1; i <= year; i++){
+        if (i % 100 == 0) {
+            if (i % 400 == 0) {
+                countLeapYear++
+                continue
+            } else {
+                continue
+            }
+        }
+        i % 4 == 0 ? countLeapYear++ : false
+    }
+
+    return countLeapYear
+}
