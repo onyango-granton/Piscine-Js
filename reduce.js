53da89ea44e00e9666efcb3f02228b970c33fa23
gonyango <grantononyango@gmail.com>
2024-09-05 16:40:29 +0300

    reduce js

---
+++ reduce.js
@@ -0,0 +1,33 @@
+const fold = function (array, func, accumulator) {
+    for (let i = 0; i < array.length; i++){
+        accumulator += func(array[i],i,array)
+    }
+    return accumulator
+}
+
+const foldRight = function (array, func, accumulator) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    accumulator += func(array[i], i, array);
+  }
+  return accumulator;
+};
+
+const reduce = function (array, func, accumulator) {
+  if (accumulator == null) {
+    accumulator = 0;
+  }
+  for (let i = array.length - 1; i >= 0; i--) {
+    accumulator += func(array[i], i, array);
+  }
+  return accumulator;
+};
+
+const reduceRight = function (array, func, accumulator) {
+    if (accumulator == null) {
+        accumulator = 0
+    }
+  for (let i = array.length - 1; i >= 0; i--) {
+    accumulator += func(array[i], i, array);
+  }
+  return accumulator;
+};
