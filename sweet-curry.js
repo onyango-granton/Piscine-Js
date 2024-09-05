fe5a762d48718e98d7d6a01f7e5061e1df47151f
gonyango <grantononyango@gmail.com>
2024-09-05 18:32:19 +0300

    currying js

---
+++ sweet-curry.js
@@ -0,0 +1,5 @@
+const mult2 = (arg) => (arg1) => arg * arg1
+
+const add3 = (arg) => (arg1) => (arg2) => arg + arg1 + arg2
+
+const sub4 = (arg) => (arg1) => (arg2) => (arg3) => arg - arg1 -arg2 - arg3
