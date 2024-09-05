6e4e4b8c197266f81115886ee1a316eb091dccf3
gonyango <grantononyango@gmail.com>
2024-09-05 14:41:49 +0300

    for each doesnt rreturn

--- for-each.js
+++ for-each.js
@@ -1,7 +1,5 @@
 const forEach = function (array, func) {
-    let res = []
     for (let i = 0; i < array.length; i++){
-        res.push(func(array[i]))
+        func(array[i])
     }
-    return res
 }
