d13c59dd3aba59b7a53e909c33e285c0883cc74a
gonyango <grantononyango@gmail.com>
2024-09-07 07:36:30 +0300

    build repair destroy

---
+++ build-brick-and-break.js
@@ -0,0 +1,28 @@
+const build = function (bricks) {
+    let bodyElem = document.getElementsByTagName('body')[0]
+    let brickId = 1
+    const buildInterval = setInterval(() => {
+        let newDiv = document.createElement('div')
+        newDiv.setAttribute('id', `brick-${brickId}`)
+            (brickId % 3 === 2) ? newDiv.setAttribute('foundation', true) : false
+        bodyElem.append(newDiv)
+        brickId++
+        (brickId == bricks + 1) ? clearInterval(buildInterval) : false
+    },100)
+}
+
+const repair = function (ids) {
+    ids.array.forEach(element => {
+        let brickDiv = document.getElementById(element)
+        brickDiv.getAttribute("foundation")
+          ? brickDiv.setAttribute("repaired", "in progress")
+          : brickDiv.setAttribute("repaired", true);
+    });
+}
+
+const destroy = function () {
+    let bricks = document.getElementsByTagName('div')
+    bricks[bricks.length - 1].remove()
+}
+
+export {build, repair, destroy}
