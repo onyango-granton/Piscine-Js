462c845127463e172bbc204bda9cebc7229c8d33
gonyango <grantononyango@gmail.com>
2024-09-07 08:29:46 +0300

    pimp my ride

---
+++ pimp-my-style.js
@@ -0,0 +1,20 @@
+var counter = 0;
+
+const pimp = function() {
+  var button = document.querySelector("button.button");
+  if (!button.classList.contains("unpimp")) {
+    button.classList.add(styles[counter]);
+    counter++;
+  } else {
+    counter--;
+    button.classList.remove(styles[counter]);
+    if (counter === 0) {
+      button.classList.toggle("unpimp");
+    }
+  }
+  if (counter === styles.length) {
+    button.classList.toggle("unpimp");
+  }
+}
+
+export { pimp };
