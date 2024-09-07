6459443630cc85113f7cfe1145a70d1ae80eb6d0
gonyango <grantononyango@gmail.com>
2024-09-07 08:40:23 +0300

    pimo me ride

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,19 +1,25 @@
-var counter = 0;
+import { styles } from "./pimp-my-style.data.js";
+
+let count = 0;
 
 const pimp = function() {
-  var button = document.querySelector("button.button");
-  if (!button.classList.contains("unpimp")) {
-    button.classList.add(styles[counter]);
-    counter++;
-  } else {
-    counter--;
-    button.classList.remove(styles[counter]);
-    if (counter === 0) {
+  const button = document.querySelector("button.button");
+  if (!button) return; // Guard clause to handle missing button
+
+  if (button.classList.contains("unpimp")) {
+    // Reverse the process
+    button.classList.remove(styles[count]);
+    count = (count - 1 + styles.length) % styles.length;
+    if (count === 0) {
       button.classList.toggle("unpimp");
     }
-  }
-  if (counter === styles.length) {
-    button.classList.toggle("unpimp");
+  } else {
+    // Apply the next style
+    button.classList.add(styles[count]);
+    count = (count + 1) % styles.length;
+    if (count === 0) {
+      button.classList.toggle("unpimp");
+    }
   }
 }
 
