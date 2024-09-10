37596fedfed91ecdc27d116d85219cf0d7cac650
gonyango <grantononyango@gmail.com>
2024-09-10 16:39:04 +0300

    filter map reduce entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,52 +1,88 @@
 const filterEntries = function (obj, func) {
   const shallowCopy = { ...obj };
-    Object.entries(shallowCopy).forEach(([key, value]) => {
-        if (!func([key, value])) {
-        delete shallowCopy[key];
+  Object.entries(shallowCopy).forEach(([key, value]) => {
+    if (!func([key, value])) {
+      delete shallowCopy[key];
     }
   });
   return shallowCopy;
 };
 
 const mapEntries = function (obj, func) {
-    const shallowCopy = { ...obj }
-    Object.entries(shallowCopy).forEach(([key, value]) => {
-        shallowCopy[func([key, value])] = shallowCopy[key]
-        delete shallowCopy[key]
-    })
-    return shallowCopy
-}
+  const shallowCopy = { ...obj };
+  Object.entries(shallowCopy).forEach(([key, value]) => {
+    shallowCopy[func([key, value])] = shallowCopy[key];
+    delete shallowCopy[key];
+  });
+  return shallowCopy;
+};
 
-const reduceEntries = function (obj, func,initialValue) {
-     let shallowCopy = { ...obj };
-    let keyArr = Object.keys(shallowCopy);
-    let valArr = Object.values(shallowCopy)
+const reduceEntries = function (obj, func, initialValue) {
+  let shallowCopy = { ...obj };
+  let keyArr = Object.keys(shallowCopy);
+  let valArr = Object.values(shallowCopy);
 
-     if (initialValue == null) {
-       initialValue = keyArr[0] + valArr[0];
-         for (let i = 1; i < keyArr.length; ) {
-             for (let j = 1; j < valArr.length; j++){
-                 initialValue = func(initialValue, [keyArr[i], `${valArr[i]}`]);
-                 i++
-             }
-             break
-       }
-     } else {
-       for (let i = 0; i < keyArr.length; ) {
-         for (let j = 0; j < valArr.length; j++) {
-           initialValue = func(initialValue, [keyArr[i], `${valArr[i]}`]);
-           i++;
-         }
-         break;
-       }
-     }
+  if (initialValue == null) {
+    initialValue = keyArr[0] + valArr[0];
+    for (let i = 1; i < keyArr.length; ) {
+      for (let j = 1; j < valArr.length; j++) {
+        initialValue = func(initialValue, [keyArr[i], `${valArr[i]}`]);
+        i++;
+      }
+      break;
+    }
+  } else {
+    for (let i = 0; i < keyArr.length; ) {
+      for (let j = 0; j < valArr.length; j++) {
+        initialValue = func(initialValue, [keyArr[i], `${valArr[i]}`]);
+        i++;
+      }
+      break;
+    }
+  }
 
-     return initialValue;
-    
-}
+  return initialValue;
+};
 
-const groceriesCart1 = { oil: 500, onion: 230, garlic: 220, paprika: 480 };
-console.log(filterEntries(groceriesCart1, ([,v]) => v < 300));
-console.log(mapEntries(groceriesCart1, function ([k, v]) { return [v > 250 ? `✔${k}` : `❌${k}`, v - 250]; }))
+const totalCalories = function (obj) {
+  let totalCalories = 0;
+  let shallowCopy = { ...obj };
+  Object.entries(shallowCopy).forEach(([key, value]) => {
+    if (key in nutritionDB) {
+      console.log(key, nutritionDB[key]);
+      totalCalories += (nutritionDB[key].calories * value) / 100;
+    }
+  });
 
-console.log(reduceEntries(groceriesCart1, (acc, [k, v]) => acc + k + v, ""));
+  return Math.round(totalCalories * 10) / 10;
+};
+
+const lowCarbs = function (obj) {
+  let shallowCopy = { ...obj };
+  Object.entries(shallowCopy).forEach(([key, value]) => {
+    let carbs = (nutritionDB[key].carbs * value) / 100;
+    if (carbs >= 50) {
+      delete shallowCopy[key];
+    }
+  });
+  return shallowCopy;
+};
+
+const cartTotal = function (obj) {
+  let shallowCopy = { ...obj };
+  let nutritionCopy = { ...nutritionDB };
+
+  Object.keys(nutritionCopy).forEach((key) => {
+    if (!(key in shallowCopy)) {
+      delete nutritionCopy[key];
+    }
+  });
+
+  Object.entries(shallowCopy).forEach(([key, value]) => {
+    Object.entries(nutritionCopy[key]).forEach(([key1, value1]) => {
+      nutritionCopy[key][key1] = (value1 * value) / 100;
+    });
+  });
+
+  return nutritionCopy;
+};
