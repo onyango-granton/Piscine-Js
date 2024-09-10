fd45f459914c803917e32cf75ad880284c9a41dc
gonyango <grantononyango@gmail.com>
2024-09-10 20:36:11 +0300

    reduce filter map scores

--- curry-entries.js
+++ curry-entries.js
@@ -1,120 +1,107 @@
 const defaultCurry = function (obj1) {
-    return function (obj2) {
-        let returnRes = {}
-        Object.entries(obj1).forEach(([key, value]) => {
-            returnRes[key] = value
-        })
-        Object.entries(obj2).forEach(([key, value]) => {
-            if (key in returnRes) {
-              returnRes[key] = value
-            } else {
-                returnRes[key] = value
-          }
-        });
-        return returnRes
-    }
-}
-
-const mapCurry = function (func) {
-    return function (obj) {
-        const shallowCopy = { ...obj };
-        Object.entries(shallowCopy).forEach(([key, value]) => {
-          shallowCopy[func([key, value])[0]] = func([key, value])[1];
-          delete shallowCopy[key];
-        });
-        return shallowCopy;
-    }
-}
-
-
-const reduceCurry = function (func) {
-    return function (obj, initialValue) {
-        let shallowCopy = { ...obj };
-        let keyArr = Object.keys(shallowCopy);
-        let valArr = Object.values(shallowCopy);
-
-        if (initialValue == null) {
-          initialValue = keyArr[0] + valArr[0];
-          for (let i = 1; i < keyArr.length; ) {
-            for (let j = 1; j < valArr.length; j++) {
-              initialValue = func(initialValue, [keyArr[i], `${valArr[i]}`]);
-              i++;
-            }
-            break;
-          }
-        } else {
-          for (let i = 0; i < keyArr.length; ) {
-            for (let j = 0; j < valArr.length; j++) {
-              initialValue = func(initialValue, [keyArr[i], valArr[i]]);
-              i++;
-            }
-            break;
-          }
-        }
-
-        return initialValue;
-    }
-}
-
-const filterCurry = function (func) {
-    return function (obj) {
-        const shallowCopy = { ...obj };
-        Object.entries(shallowCopy).forEach(([key, value]) => {
-          if (!func([key, value])) {
-            delete shallowCopy[key];
-          }
-        });
-        return shallowCopy;
-    }
-}
-
-console.log(
-  defaultCurry({
-    http: 403,
-    connection: "close",
-    contentType: "multipart/form-data",
-  })({
-    http: 200,
-    connection: "open",
-    requestMethod: "GET",
-  })
-);
-
-
-const personnel = {
-  lukeSkywalker: {
-    id: 5,
-    pilotingScore: 98,
-    shootingScore: 56,
-    isForceUser: true,
-  },
-  sabineWren: {
-    id: 82,
-    pilotingScore: 73,
-    shootingScore: 99,
-    isForceUser: false,
-  },
-  zebOrellios: {
-    id: 22,
-    pilotingScore: 20,
-    shootingScore: 59,
-    isForceUser: false,
-  },
-  ezraBridger: {
-    id: 15,
-    pilotingScore: 43,
-    shootingScore: 67,
-    isForceUser: true,
-  },
-  calebDume: {
-    id: 11,
-    pilotingScore: 71,
-    shootingScore: 85,
-    isForceUser: true,
-  },
+  return function (obj2) {
+    let returnRes = {};
+    Object.entries(obj1).forEach(([key, value]) => {
+      returnRes[key] = value;
+    });
+    Object.entries(obj2).forEach(([key, value]) => {
+      if (key in returnRes) {
+        returnRes[key] = value;
+      } else {
+        returnRes[key] = value;
+      }
+    });
+    return returnRes;
+  };
 };
 
-console.log(reduceCurry((acc, [k, v]) => acc.concat(" ", `${k}:${v.id}`))(
-  personnel,
-  "personnel:"
-))
+const mapCurry = function (func) {
+  return function (obj) {
+    const shallowCopy = { ...obj };
+    Object.entries(shallowCopy).forEach(([key, value]) => {
+      shallowCopy[func([key, value])[0]] = func([key, value])[1];
+      delete shallowCopy[key];
+    });
+    return shallowCopy;
+  };
+};
+
+const reduceCurry = function (func) {
+  return function (obj, initialValue) {
+    let shallowCopy = { ...obj };
+    let keyArr = Object.keys(shallowCopy);
+    let valArr = Object.values(shallowCopy);
+
+    if (initialValue == null) {
+      initialValue = keyArr[0] + valArr[0];
+      for (let i = 1; i < keyArr.length; ) {
+        for (let j = 1; j < valArr.length; j++) {
+          initialValue = func(initialValue, [keyArr[i], `${valArr[i]}`]);
+          i++;
+        }
+        break;
+      }
+    } else {
+      for (let i = 0; i < keyArr.length; ) {
+        for (let j = 0; j < valArr.length; j++) {
+          initialValue = func(initialValue, [keyArr[i], valArr[i]]);
+          i++;
+        }
+        break;
+      }
+    }
+
+    return initialValue;
+  };
+};
+
+const filterCurry = function (func) {
+  return function (obj) {
+    const shallowCopy = { ...obj };
+    Object.entries(shallowCopy).forEach(([key, value]) => {
+      if (!func([key, value])) {
+        delete shallowCopy[key];
+      }
+    });
+    return shallowCopy;
+  };
+};
+
+const reduceScore = function (obj, initialValue) {
+  initialValue = null ? (initialValue = 0) : null;
+  Object.entries(obj).forEach(([key]) => {
+    if (obj[key].isForceUser) {
+      Object.entries(obj[key]).forEach(([key, value]) => {
+        if (key != "id" && key != "isForceUser") {
+          initialValue += value;
+        }
+      });
+    }
+  });
+  return initialValue;
+};
+
+const filterForce = function (obj) {
+  let shallowCopy = { ...obj };
+  Object.entries(shallowCopy).forEach(([key]) => {
+    if (shallowCopy[key].shootingScore < 80) {
+      delete shallowCopy[key];
+    }
+  });
+  return shallowCopy;
+};
+
+const mapAverage = function (obj) {
+  let shallowCopy = { ...obj };
+  Object.entries(shallowCopy).forEach(([key, value]) => {
+    let keySum = 0;
+    Object.entries(shallowCopy[key]).forEach(([key, value]) => {
+      if (key != "id" && key != "isForceUser") {
+        keySum += value;
+      }
+    });
+    shallowCopy[key].avarage = keySum / 2;
+  });
+
+  return shallowCopy;
+};
