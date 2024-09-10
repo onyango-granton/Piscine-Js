cea0d7c871826b0c78507c2a9e4127c8e8916939
gonyango <grantononyango@gmail.com>
2024-09-10 10:53:10 +0300

    removed multiple initial value

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -1,39 +1,36 @@
 const filterKeys = function (obj, func) {
-    let shallowCopy = { ...obj }
-    Object.keys(shallowCopy).forEach(key => {
-        if (!(func(key))) {
-            delete shallowCopy[key]
-        }
-    })
-    return shallowCopy
-}
+  let shallowCopy = { ...obj };
+  Object.keys(shallowCopy).forEach((key) => {
+    if (!func(key)) {
+      delete shallowCopy[key];
+    }
+  });
+  return shallowCopy;
+};
 
 const mapKeys = function (obj, func) {
-    let shallowCopy = { ...obj }
-    Object.keys(shallowCopy).forEach(key => {
-        shallowCopy[func(key)] = shallowCopy[key]
-        delete shallowCopy[key]
-    })
-    return shallowCopy
-}
+  let shallowCopy = { ...obj };
+  Object.keys(shallowCopy).forEach((key) => {
+    shallowCopy[func(key)] = shallowCopy[key];
+    delete shallowCopy[key];
+  });
+  return shallowCopy;
+};
 
 const reduceKeys = function (obj, func, initialValue) {
-    let shallowCopy = { ...obj }
-    let keyArr = Object.keys(shallowCopy)
-    initialValue == null ? initialValue = keyArr[0] : null
+  let shallowCopy = { ...obj };
+  let keyArr = Object.keys(shallowCopy);
 
-    if (initialValue == null) {
-        initialValue = keyArr[0]
-        for (let i = 1; i < keyArr.length; i++) {
-          initialValue = func(initialValue, keyArr[i]);
-        }
-    } else {
-        for (let i = 0; i < keyArr.length; i++) {
-          initialValue = func(initialValue, keyArr[i]);
-        }
+  if (initialValue == null) {
+    initialValue = keyArr[0];
+    for (let i = 1; i < keyArr.length; i++) {
+      initialValue = func(initialValue, keyArr[i]);
     }
+  } else {
+    for (let i = 0; i < keyArr.length; i++) {
+      initialValue = func(initialValue, keyArr[i]);
+    }
+  }
 
-    return initialValue
-    
-
-}
+  return initialValue;
+};
