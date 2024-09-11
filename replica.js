d9f6e6646614bc7e96e3c40cf056d95955e8b12d
gonyango <grantononyango@gmail.com>
2024-09-11 13:26:37 +0300

    replica js

--- replica.js
+++ replica.js
@@ -12,15 +12,31 @@ const isObject = (n) =>
 const isFunction = (n) => isType(n, "function");
 
 function replica(target, ...sources) {
-  for (const source of sources) {
-    for (const [key, value] of Object.entries(source)) {
-      if (isObject(value)) {
-        target[key] = target[key] ?? {};
-        replica(target[key], value);
+  sources.forEach((source) => {
+    Object.keys(source).forEach((key) => {
+      if (isObject(source[key])) {
+        if (!target.hasOwnProperty(key) || !isObject(target[key])) {
+          target[key] = {};
+        }
+        replica(target[key], source[key]);
       } else {
-        target[key] = value;
+        target[key] = source[key];
       }
-    }
-  }
+    });
+  });
   return target;
 }
+
+console.log(
+  replica(
+    {},
+    Object.freeze({ line: "Replicants are like any other machine" }),
+    Object.freeze({ author: "Rich" })
+  )
+);
+
+console.log(replica({ con: console.log }, { reg: /hello/ }));
+
+console.log(replica({ a: 4 }, { a: { b: 1 } }).a.b);
+
+console.log(replica({ a: 2 }, { a: [4] }).a);
