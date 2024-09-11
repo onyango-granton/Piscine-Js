935e8ba2edbca445c5323a1b5abc3f99d00b101d
gonyango <grantononyango@gmail.com>
2024-09-11 05:45:06 +0300

    handling freeze

--- deep-copy.js
+++ deep-copy.js
@@ -1,7 +1,19 @@
 const deepCopy = function (obj) {
   if (Array.isArray(obj)) {
-    return [...obj];
+    return Object.freeze([...obj]);
   } else {
-    return { ...obj };
+    return Object.freeze({ ...obj });
   }
 };
+
+
+const test11 = function () {
+  const r = Math.random();
+  const obj = [r, Object.freeze([r, Object.freeze([r])])];
+  const copy = deepCopy(obj);
+  //eq(copy, obj);
+  console.log(obj[1][1], copy[1][1])
+  return obj[1][1] !== copy[1][1];
+}
+
+console.log(test11())
