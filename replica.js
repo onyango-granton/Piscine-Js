6e3f38af5bbec88749799945f6e77a5672465b24
gonyango <grantononyango@gmail.com>
2024-09-11 08:03:02 +0300

    replica func

--- replica.js
+++ replica.js
@@ -1,19 +1,6 @@
 function replica(target, ...sources) {
   sources.forEach((source) => {
-    Object.keys(source).forEach((key) => {
-      if (
-        typeof source[key] === "object" &&
-        source[key] !== null &&
-        !Array.isArray(source[key])
-      ) {
-        if (!target[key]) {
-          target[key] = {};
-        }
-        replica(target[key], source[key]);
-      } else {
-        target[key] = source[key];
-      }
-    });
+    target = Object.assign(target, source);
   });
   return target;
 }
