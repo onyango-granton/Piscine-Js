68557c43dc7243fd23c6c8adf4c551b7bea291e1
gonyango <grantononyango@gmail.com>
2024-09-11 08:42:56 +0300

    refactor replica code

--- replica.js
+++ replica.js
@@ -1,6 +1,8 @@
 function replica(target, ...sources) {
+  const result = Object.assign({}, target);
   sources.forEach((source) => {
-    target = Object.assign(target, source);
+    result.a = Object.assign({}, result.a, source.a); 
   });
-  return target;
+  return result;
 }
+
