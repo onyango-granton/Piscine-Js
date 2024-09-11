0411128f93bfdba9a7c273c53eddf028ca13453c
gonyango <grantononyango@gmail.com>
2024-09-11 13:43:32 +0300

    replica js

--- replica.js
+++ replica.js
@@ -1,42 +1,20 @@
-function isType(n, type) {
-  return typeof n === type;
-}
-
-const isArray = (n) => isType(n, "array");
-const isObject = (n) =>
-  isType(n, "object") &&
-  !isFunction(n) &&
-  !isArray(n) &&
-  n !== null &&
-  !(n instanceof RegExp);
-const isFunction = (n) => isType(n, "function");
-
-function replica(target, ...sources) {
-  sources.forEach((source) => {
-    Object.keys(source).forEach((key) => {
-      if (isObject(source[key])) {
-        if (!target.hasOwnProperty(key) || !isObject(target[key])) {
-          target[key] = {};
-        }
-        replica(target[key], source[key]);
+const isObject = (a) =>
+  typeof a === "object" &&
+  !(
+    a instanceof Set ||
+    a instanceof RegExp ||
+    a instanceof Array ||
+    a instanceof Map
+  );
+const replica = function (target, ...sources) {
+  sources.forEach (source => {
+    Object.entries(source).forEach(([key, value]) => {
+      if (isObject(value) && isObject(target[key])) {
+        replica(target[key], value);
       } else {
-        target[key] = source[key];
+        target[key] = value;
       }
     });
-  });
+  })
   return target;
-}
-
-console.log(
-  replica(
-    {},
-    Object.freeze({ line: "Replicants are like any other machine" }),
-    Object.freeze({ author: "Rich" })
-  )
-);
-
-console.log(replica({ con: console.log }, { reg: /hello/ }));
-
-console.log(replica({ a: 4 }, { a: { b: 1 } }).a.b);
-
-console.log(replica({ a: 2 }, { a: [4] }).a);
+};
