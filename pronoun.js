f201ca2746e2440dd41f9d5cb0df0e989c1def78
gonyango <grantononyango@gmail.com>
2024-09-11 15:23:18 +0300

    refactor pronoun func

--- pronoun.js
+++ pronoun.js
@@ -1,12 +1,13 @@
-const regexPronoun = /i\s+|you\s+|he\s+|she\s+|it\s+|they\s+|we\s+/i;
+const regexPronoun =
+  /\s+i\s+|\s+you\s+|\s+he\s+|\s+she\s+|\s+it\s+|\s+they\s+|\s+we\s+/i;
 
 function pronoun(string) {
   const result = {};
   const words = string.split(" ");
 
   for (let i = 0; i < words.length; i++) {
-    const match = regexPronoun.exec(words[i]+" ");
-      if (match && !regexPronoun.exec(words[i + 1] + " ") && !words[i + 1] == "") {
+    const match = regexPronoun.exec(" "+words[i]+" ");
+      if (match && !regexPronoun.exec(" "+words[i + 1] + " ") && !words[i + 1] == "") {
         //console.log(match);
         const pronoun = match[0].trim();
         const nextWord = words[i + 1] || ""; 
@@ -18,6 +19,12 @@ function pronoun(string) {
 
   return result;
 }
+
+console.log(
+  pronoun(`Your reducer function's returned value is assigned to the accumulator,
+whose value is remembered across each iteration throughout the array and
+ultimately becomes the final, single resulting value.`)
+);
 
 const ex = 'Using Array Destructuring, you you can iterate through objects easily.'
 console.log(pronoun(ex))
