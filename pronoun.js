797f7680618f8573c07d2a7bc51d03b057aeee46
gonyango <grantononyango@gmail.com>
2024-09-11 18:35:01 +0300

    refactor pronoun js

--- pronoun.js
+++ pronoun.js
@@ -2,34 +2,47 @@ const regexPronoun =
   /\s+i\s+|\s+you\s+|\s+he\s+|\s+she\s+|\s+it\s+|\s+they\s+|\s+we\s+/i;
 
 function pronoun(string) {
+  let arr = [];
   const result = {};
-  const words = string.split(" ");
+  let words = string.split(" ");
+  words.forEach((element) => {
+    if (element.includes("\n")) {
+      let newWords = element.split("\n");
+      arr = arr.concat(newWords);
+    } else {
+      arr.push(element);
+    }
+  });
+
+  words = arr;
+
+  arr = [];
+
+  words.forEach((word) => {
+    word = word.toLowerCase();
+    arr.push(word);
+  });
+
+  words = arr;
 
   for (let i = 0; i < words.length; i++) {
-    const match = regexPronoun.exec(" "+words[i]+" ");
-      if (match && !regexPronoun.exec(" "+words[i + 1] + " ") && !words[i + 1] == "") {
-        //console.log(match);
-        const pronoun = match[0].trim();
-        const nextWord = words[i + 1] || ""; 
+    const match = regexPronoun.exec(" " + words[i] + " ");
+
+    if (match) {
+      const pronoun = match[0].trim();
+      const nextWord = words[i + 1];
+      if (nextWord == undefined) {
+        result[pronoun] = { word: [], count: 0 };
+      } else {
         result[pronoun] = result[pronoun] || { word: [], count: 0 };
-        result[pronoun].word.push(nextWord);
-        result[pronoun].count++;
+        if (!regexPronoun.exec(" " + words[i + 1] + " ")) {
+          result[pronoun].word.push(nextWord);
+        }
       }
+
+      result[pronoun].count++;
+    }
   }
 
   return result;
 }
-
-console.log(
-  pronoun(`Your reducer function's returned value is assigned to the accumulator,
-whose value is remembered across each iteration throughout the array and
-ultimately becomes the final, single resulting value.`)
-);
-
-const ex = 'Using Array Destructuring, you you can iterate through objects easily.'
-console.log(pronoun(ex))
-
-
-const ex2 = 'If he you want to buy something you have to pay.'
-
-console.log(pronoun(ex2))
