3229df0ab660db3d04d22f8ba0bb86a566e79cad
gonyango <grantononyango@gmail.com>
2024-09-11 09:46:48 +0300

    replica js

--- replica.js
+++ replica.js
@@ -4,7 +4,12 @@ function deepMerge(target, ...sources) {
 
   for (const key in source) {
     if (source.hasOwnProperty(key)) {
-      if (typeof target[key] === "object" && typeof source[key] === "object") {
+      if (Array.isArray(target[key]) && Array.isArray(source[key])) {
+        target[key] = target[key].concat(source[key]); // Concatenate arrays
+      } else if (
+        typeof target[key] === "object" &&
+        typeof source[key] === "object"
+      ) {
         target[key] = deepMerge(target[key] || {}, source[key]);
       } else {
         target[key] = source[key];
