c6cf6da6918b74d795ee60e620fc53b7837716f4
gonyango <grantononyango@gmail.com>
2024-09-11 13:48:27 +0300

    replica js

--- replica.js
+++ replica.js
@@ -1,26 +1,19 @@
-function isType(n, type) {
-  return typeof n === type;
-}
+const isObject = (a) =>
+  typeof a === "object" &&
+  !(
+    a instanceof Set ||
+    a instanceof RegExp ||
+    a instanceof Array ||
+    a instanceof Map
+  );
 
-const isArray = (n) => isType(n, "array");
-const isObject = (n) =>
-  isType(n, "object") &&
-  !isFunction(n) &&
-  !isArray(n) &&
-  n !== null &&
-  !(n instanceof RegExp);
-const isFunction = (n) => isType(n, "function");
-
-const replica = function (target, ...sources) {
+const replica = (target, ...sources) => {
   sources.forEach((source) => {
     Object.entries(source).forEach(([key, value]) => {
-      if (isObject(value) && isObject(target[key])) {
-        replica(target[key], value);
-      } else {
-        target[key] = value;
-      }
+      if (isObject(value) && isObject(target[key]))
+        target[key] = replica(target[key], value);
+      else target[key] = value;
     });
   });
   return target;
 };
-;
