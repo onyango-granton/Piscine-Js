7a1915233af9d7465cbb6c5f250b05432e57f837
gonyango <grantononyango@gmail.com>
2024-09-12 14:00:43 +0300

    debounce func

--- debounce.js
+++ debounce.js
@@ -1,10 +1,10 @@
 const debounce = function (func, waitTime) {
   let time = Date.now()
-  let stopTime = time + waitTime
+  let stopTime = time + waitTime - 1000
 
   while (true){
     let countTime = Date.now()
-    if (countTime == stopTime) {
+    if (countTime >= stopTime) {
       return func
     }
   }
