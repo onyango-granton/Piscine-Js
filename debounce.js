46de42493e337ab6746c4e880bdba42755d414c4
gonyango <grantononyango@gmail.com>
2024-09-12 16:41:21 +0300

    debounce and opBounce

--- debounce.js
+++ debounce.js
@@ -6,3 +6,26 @@ function debounce(func, waitTime) {
     timeout = setTimeout(() => func.apply(this, args), waitTime);
   };
 }
+
+function opDebounce(func, waitTime, options = {}) {
+  let timeout;
+  let leadingCalled = false;
+
+  return function (...args) {
+    const callNow = options.leading && !leadingCalled;
+
+    clearTimeout(timeout);
+
+    if (callNow) {
+      func.apply(this, args);
+      leadingCalled = true;
+    }
+
+    timeout = setTimeout(() => {
+      if (!options.leading) {
+        func.apply(this, args);
+      }
+      leadingCalled = false;
+    }, waitTime);
+  };
+}
