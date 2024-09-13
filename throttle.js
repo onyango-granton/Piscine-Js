7723619cb8abba4184711b7057f4a6459a6d0399
gonyango <grantononyango@gmail.com>
2024-09-13 13:47:36 +0300

    throttle func

--- throttle.js
+++ throttle.js
@@ -1,17 +1,46 @@
-
-function debounce(func, waitTime) {
+function opDebounce(func, waitTime, options = {}) {
   let timeout;
+  let leadingCalled = false;
 
   return function (...args) {
+    const callNow = options.leading && !leadingCalled;
+
     clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), waitTime);
+
+    if (callNow) {
+      func.apply(this, args);
+      leadingCalled = true;
+    }
+
+    timeout = setTimeout(() => {
+      if (!options.leading) {
+        func.apply(this, args);
+      }
+      leadingCalled = false;
+    }, waitTime);
   };
 }
 
-const throttle = function (func, wait) {
-    return debounce(func,wait)
+const isObject = function (obj) {
+    return typeof(obj) === 'object' && !Array.isArray(obj)
 }
 
-const opThrottle = function (func, wait, options) {
-    
+function throttle(func, wait, options) {
+  var leading = true,
+    trailing = true;
+
+  if (typeof func != "function") {
+    throw new TypeError(FUNC_ERROR_TEXT);
+  }
+  if (isObject(options)) {
+    leading = "leading" in options ? !!options.leading : leading;
+    trailing = "trailing" in options ? !!options.trailing : trailing;
+  }
+  return opDebounce(func, wait, {
+    leading: leading,
+    maxWait: wait,
+    trailing: trailing,
+  });
 }
+
+const opThrottle = function (func, wait, options) {};
