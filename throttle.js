de1a333efef25712575d18e952be4d1aa82c2bfb
gonyango <grantononyango@gmail.com>
2024-09-14 08:49:23 +0300

    opthrottle

--- throttle.js
+++ throttle.js
@@ -1,12 +1,37 @@
 const throttle = function (func, delay) {
+  let timeout;
+  return (...args) => {
+    if (!timeout) {
+      func(...args);
+      timeout = setTimeout(() => {
+        timeout = null;
+      }, delay);
+    }
+  };
+};
+
+
+const opThrottle = function (func, delay, options = {}) {
     let timeout
+    let leadingCalled = false
+
     return (...args) => {
-        if (!timeout) {
-            func(...args);
-            timeout = setTimeout(() => {
-                timeout = null
-            }, delay);
+        const callNow = options.leading && !leadingCalled;
+
+        if (callNow) {
+          func(...args);
+          leadingCalled = true;
         }
-        
+
+        if (!timeout) {
+            if (!options.leading) {
+                func(...args);
+            }
+          
+          timeout = setTimeout(() => {
+              timeout = null;
+              leadingCalled = false
+          }, delay);
+        }
     }
 }
