345a0e683fa6bf94c4f27e00454f5feba4444531
gonyango <grantononyango@gmail.com>
2024-09-14 13:23:38 +0300

    is winner func

---
+++ is-winner.js
@@ -0,0 +1,24 @@
+async function isWinner(country) {
+  try {
+    const winnerData = await db.getWinner(country);
+
+    if (!winnerData) {
+      return `${country} never was a winner`;
+    }
+
+    const results = await db.getResults(winnerData.id);
+
+    if (results.length < 3) {
+      return `${country} is not what we are looking for because of the number of times it was champion`;
+    }
+
+    const years = results.map((result) => result.year).join(", ");
+    const scores = results.map((result) => result.score).join(", ");
+
+    return `${country} won the FIFA World Cup in ${years} winning by ${scores}`;
+  } catch (error) {
+    // Handle potential errors from db.getWinner or db.getResults
+    console.error(`Error getting winner info for ${country}`, error);
+    return `Error retrieving information about ${country}`;
+  }
+}
