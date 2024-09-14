49d7065b6a2c5a04405ffab9993beda1bf86d701
gonyango <grantononyango@gmail.com>
2024-09-14 14:59:14 +0300

    all funciton

---
+++ all.js
@@ -0,0 +1,14 @@
+function all(object) {
+  const entries = Object.entries(object);
+
+  return Promise.all(
+    entries.map(async ([key, value]) => {
+      try {
+        const resolvedValue = await value;
+        return [key, resolvedValue];
+      } catch (error) {
+        throw error; // Re-throw to be caught by Promise.all
+      }
+    })
+  ).then((resolvedEntries) => Object.fromEntries(resolvedEntries));
+}
