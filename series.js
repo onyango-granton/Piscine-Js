5b792b0edd26f113d4821f1a85dfe153f3060b86
gonyango <grantononyango@gmail.com>
2024-09-14 15:56:29 +0300

    series func

--- series.js
+++ series.js
@@ -1,8 +1,10 @@
 async function series(funcArray) {
+    let resultArr = []
     if (funcArray.length == 0) {
         return []
     }
     for (let i = 0; i < funcArray.length; i++){
-        await funcArray[i]
+       await resultArr.push(await funcArray[i])
     }
+    return resultArr
 }
