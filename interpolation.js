67f8ed33d5f75f7db60b56bb273760a90a754ff6
gonyango <grantononyango@gmail.com>
2024-09-14 10:27:21 +0300

    setinterval time

--- interpolation.js
+++ interpolation.js
@@ -2,6 +2,6 @@ const interpolation = function ({ step, start, end, callback, duration }) {
     for (let i = start; i < end; i++){
         let x = start + (end - start) * (i / step)
         let y = duration / step * (i + 1)
-        setTimeout(() => {callback([x,y])})
+        setTimeout(() => {callback([x,y])},y)
     }
 }
