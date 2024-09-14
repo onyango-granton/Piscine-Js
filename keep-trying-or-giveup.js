4c7cdcfacc37337df36dfbec3086792bdea1b692
gonyango <grantononyango@gmail.com>
2024-09-14 12:22:41 +0300

    timeout func

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,13 +1,26 @@
 const retry = function (count, callback) {
-    return async (...args) => {
-        let catchError
-        for (let i = 0; i <= count; i++){
-            try {
-              return await callback(...args);
-            } catch (e) {
-                catchError = e
-            }
-        }   
-        throw catchError
+  return async (...args) => {
+    let catchError;
+    for (let i = 0; i <= count; i++) {
+      try {
+        return await callback(...args);
+      } catch (e) {
+        catchError = e;
+      }
     }
-}
+    throw catchError;
+  };
+};
+
+const timeout = function (delay, callback) {
+  return (...args) => {
+    let newErr;
+    try {
+      return setTimeout(() => callback(...args), delay);
+    } catch (e) {
+      newErr = e;
+    }
+
+    throw newErr;
+  };
+};
