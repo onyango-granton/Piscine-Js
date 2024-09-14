5407af61fa3f2041bedb5c51dc280e606eb589df
gonyango <grantononyango@gmail.com>
2024-09-14 13:25:36 +0300

    winnin

--- is-winner.js
+++ is-winner.js
@@ -19,6 +19,6 @@ async function isWinner(country) {
   } catch (error) {
     // Handle potential errors from db.getWinner or db.getResults
     console.error(`Error getting winner info for ${country}`, error);
-    return `Error retrieving information about ${country}`;
+    return `${country} was never a winner`;
   }
 }
