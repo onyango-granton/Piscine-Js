ae4f32d7e07764cde12945bf97e0706eb5919173
gonyango <grantononyango@gmail.com>
2024-09-14 10:22:24 +0300

    interpolaion

--- interpolation.js
+++ interpolation.js
@@ -1,20 +1,7 @@
-function interpolation({ step, start, end, callback, duration }) {
-  const interval = duration / step;
-  const stepSize = (end - start) / step;
-
-  let currentStep = 0;
-
-  const interpolateStep = () => {
-    if (currentStep < step) {
-      const x = currentStep; 
-      const y = start + currentStep * stepSize; 
-
-      callback([x, y]); 
-      currentStep++;
-
-      setTimeout(interpolateStep, interval); 
+const interpolation = function ({ step, start, end, callback, duration }) {
+    for (let i = start; i < end; i++){
+        let x = start + (end - start) * (i / step)
+        let y = duration / step * (i + 1)
+        setTimeout(() => {callback([x,y])})
     }
-  };
-
-  interpolateStep();
 }
