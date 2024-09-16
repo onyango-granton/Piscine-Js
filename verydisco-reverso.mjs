425354e39711659903dd9616e85436ad47a4845a
gonyango <grantononyango@gmail.com>
2024-09-16 16:27:21 +0300

    reverso using floor

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -9,10 +9,7 @@ try {
     let wordArr = []
     let splitWords = content.split(" ")
     splitWords.forEach((word) => {
-        let midVal =
-          word.length % 2 == 0
-            ? Math.ceil(word.length / 2)
-            : Math.ceil(word.length / 2) - 1;
+        let midVal = Math.floor(word.length/2)
         let partOne = word.slice(0, midVal)
         let partTwo = word.slice(midVal)
         let newWord = partTwo.concat(partOne)
